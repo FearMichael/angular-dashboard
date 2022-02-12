97a4547bcaf50ac8763b2906cd17a4e40cbdba7c
Michael Fearnley <44407940+FearMichael@users.noreply.github.com>
2022-02-12 13:45:43 -0500

    export items

--- src/app/dashboard/table/table.component.ts
+++ src/app/dashboard/table/table.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { ITableHeader } from 'src/app/interfaces/table.interfaces';
 
 @Component({
   selector: 'app-table',
@@ -6,6 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./table.component.scss']
 })
 export class TableComponent implements OnInit {
+
+  private _data: Record<string, any>[] = [];
+  public get data() {
+    return this._data;
+  }
+  @Input() public set data(val) {
+    this._data = val;
+  }
+
+  private _headers: ITableHeader[] = [];
+  public get headers() {
+    return this._headers;
+  }
+  @Input() public set headers(val) {
+    this._headers = val;
+  }
+
+  @Output() recordChange = new EventEmitter<Record<string, any>>();
 
   constructor() { }
 
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
 
 const modules = [
   MatToolbarModule,
-  MatButtonModule
+  MatButtonModule,
+  MatMenuModule
 ];
 
 @NgModule({
